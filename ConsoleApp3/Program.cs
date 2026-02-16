f3ef00ebee9943e6f66c7328e7549ee0d46fffae
KoleszarE <KoleszarE@310-13>
2026-02-16 10:16:45 +0100

    Második feladat

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,6 +8,12 @@ namespace ConsoleApp3
         public List<int> korok { get; set; }
     }
 
+    class Diak
+    {
+        public String nev { get; set; }
+        public List<int> jegyek { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -21,6 +27,24 @@ namespace ConsoleApp3
             }
             //első eletkor
             Console.WriteLine($" {adat.nevek[0]} életkor: {adat.korok[0]}");
+
+            //diakok.json
+            fajl = File.ReadAllText("diakok.json", System.Text.Encoding.Latin1);
+            Console.WriteLine(fajl);
+            List<Diak> diakok = JsonSerializer.Deserialize<List<Diak>>(fajl);
+            Console.WriteLine("Keresett név:");
+            String neve = Console.ReadLine();
+            bool megvan = false;
+            foreach (var diak in diakok)
+            {
+                if (diak.nev == neve)
+                {
+                    Console.WriteLine("Átlag:" + diak.jegyek.Average());
+                    megvan = true;
+                }
+            }
+            if(megvan == false)
+                Console.WriteLine("Nincs ilyen nevű diák!");
         }
     }
 }
