b16346b8ab986666e1af8b150e16a5b14767816b
KoleszarE <KoleszarE@310-13>
2026-02-16 09:33:40 +0100

    Első feladat

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ConsoleApp3
+﻿using System.Text.Json;
+
+namespace ConsoleApp3
 {
+    class Adat
+    {
+        public List<string> nevek { get; set; }
+        public List<int> korok { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           String fajl = File.ReadAllText("Adat.json", System.Text.Encoding.Latin1);
+            Console.WriteLine(fajl);
+            Adat adat = JsonSerializer.Deserialize<Adat>(fajl);
+            foreach (var nev in adat.nevek)
+            {
+                Console.WriteLine(nev);
+            }
+            //első eletkor
+            Console.WriteLine($" {adat.nevek[0]} életkor: {adat.korok[0]}");
         }
     }
 }
